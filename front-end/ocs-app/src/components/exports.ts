69e50c84fa2c3bb6368cbb1538e799c758ca4cb9
pezhmantp <pezhman.tabeshi@gmail.com>
2025-03-09 15:19:31 +0330

    Navbar and home components created

---
+++ front-end/ocs-app/src/components/exports.ts
@@ -0,0 +1,3 @@
+export const logo = require('../../src/images/logo.png');
+export const banner = require('../../src/images/banner.png');
+export const defaultImg = require('../../src/images/defaultImg.png');
---
+++ front-end/ocs-app/src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {logo} from '../exports'
+import HomeIcon from '@mui/icons-material/Home';
+import './navbar.css';
+import { Button } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <img src={logo} className='logo'/>
+            <div>
+            <Button id='home' startIcon={<HomeIcon style={{color:"white",fontSize:"28px"}} id='homeIcon' />}/>
+            <Button  id='userPanel' startIcon={<AccountCircleIcon style={{color:"white",fontSize:"28px"}} id="userPanelIcon" />}/>
+            
+            </div>
+    </div>
+  )
+}
+
+export default Navbar
