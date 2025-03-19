d07bca4b4b9a76c178c81ada079da3b2cecc2f8d
pezhmantp <pezhman.tabeshi@gmail.com>
2025-03-19 15:09:07 +0330

    Online clothing store backe-end app initialized

---
+++ back-end/src/main/java/com/online_store/online_store/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.online_store.online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
